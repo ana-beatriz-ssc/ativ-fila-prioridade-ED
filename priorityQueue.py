ca5aa8b37a8712f363c227a1c330da524494912e
ana-beatriz-ssc <anabeatrizsantosscontato@gmail.com>
2025-04-16 11:39:01 -0300

    Enqueue

--- priorityQueue.py
+++ priorityQueue.py
@@ -1,4 +1,52 @@
 from node import Node
 
 class priorityQueue:
-    pass
+    def __init__(self):
+        self.head = None #início da fila
+        self.tail = None #final da fila
+        self.sizeN = 0 #pessoas na fila normal
+        self.sizeP = 0 #pessoas na fila prioritária
+
+    def enqueue(self, name, priority):
+        new_node = Node(name, priority)
+
+        if (self.tail != None): #se o "rabo" da fila tem alguem, o proximo dele passa a ser o novo no
+            self.tail.next = new_node
+        self.tail = new_node  
+
+        if (self.head == None):
+            self.head = new_node
+
+        if (priority == True):
+            self.size_priority += 1
+
+        else:
+            self.size_normal += 1
+    
+    def dequeue(self):
+        if (self.head == None):
+            return None
+        
+        node_remove = self.head
+        self.head = self.head.next
+
+        if (self.head == None):
+            self.tail = None
+
+        
+
+
+    def list(self): #listar todas as pessoas da fila
+        current = self.head #current recebe o nó do início
+        while current: #enquanto  houver nós na fila
+            if current.priority:
+                tipo = "Prioridade"
+            else:
+                tipo = "Normal"
+            print(current.name + " - " + tipo) 
+            current = current.next
+
+    def is_empty(self):
+        return self.head is None
+
+            
